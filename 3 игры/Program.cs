a3eee39cd5433d3ef46e7117d5f4e9380f8dd8ae
Hotabich227 <isip_i.i.anishin@mpt.ru>
2022-10-10 13:12:23 +0300

    Практические

---
+++ 3 игры/Program.cs
@@ -0,0 +1,89 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int a;
+        do
+        {
+            Console.WriteLine(" " + '\n' + "Выберете штуку, которую хотите запустить: " + '\n' + "1. Игрулька 'Угадай число'" +
+            '\n' + "2. Таблица умножения" + '\n' + "3. Вывод делителей числа" + '\n' + "4. Закрыть программу" + '\n' + " ");
+            string oparation = Console.ReadLine();
+            a = Convert.ToInt32(oparation);
+            if (a == 1)
+            {
+                Guess_the_number();
+            }
+
+            if (a == 2)
+            {
+                Multiplication_Table();
+            }
+
+            if (a == 3)
+            {
+                Divisor_number();
+            }
+
+        } while (a != 4);
+    }
+
+    static void Guess_the_number()
+    {
+        Console.WriteLine("Угадайте число от 0 до 100 погнали!");
+        Random number = new Random();
+        int k = number.Next(0, 100);
+        int b;
+        do
+        {
+            b = Convert.ToInt32(Console.ReadLine());
+            if (b > k)
+            {
+                Console.WriteLine("Меньше");
+            }
+            if (b < k)
+            {
+                Console.WriteLine("Больше");
+            }
+        } while (k != b);
+        Console.WriteLine("Хорош это было нереально трудно!");
+    }
+
+    static void Multiplication_Table()
+    {
+        Console.WriteLine();
+        Console.WriteLine("Таблица умножения: ");
+    
+        int[,] table1 = new int[10, 10];
+        for (int i = 1; i < table1.GetLength(0); i++)
+        {
+            for (int n = 1; n < table1.GetLength(1); n++)
+            {
+                Console.Write(table1[i, n] + (i * n) + "\t");
+            }
+            Console.WriteLine();
+        }
+        
+    }
+
+    static void Divisor_number()
+    {
+        string str = "";
+        Console.WriteLine("");
+        Console.WriteLine("Введите число: ");
+        int y = Convert.ToInt32(Console.ReadLine());
+        List<int> numbers = new List<int>();
+        for (int i = 1; i < y; i++)
+        {
+            if (y % i == 0)
+            {
+                numbers.Add(i);
+            }
+        }
+        foreach (int i in numbers)
+        {
+            str = str + i.ToString() + '\t';
+        }
+        Console.WriteLine("");
+        Console.WriteLine("Ты сможешь " + y.ToString() + " разделить на: " + '\n' + str);
+    }
+}
---
+++ Практическая Калькулятор/Program.cs
@@ -0,0 +1,63 @@
+﻿int oper;
+do
+{
+    Console.Write("Введите номер операции: 1. Сложить 2 числа 2. Вычесть первое из второго 3. Перемножить два числа 4. Разделить первое на второе 5. Возвести в степень N первое число 7. Найти 1 процент от числа 8. Найти факториал из числа 9. Выйти из программы ");
+    oper = Convert.ToInt32(Console.ReadLine());
+    switch (oper)
+    {
+        case 1:
+            Console.WriteLine("Введите два числа");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(num1 + num2);
+            break;
+        case 2:
+            Console.WriteLine("Введите два числа");
+            int num3 = Convert.ToInt32(Console.ReadLine());
+            int num4 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(num3 - num4);
+            break;
+        case 3:
+            Console.WriteLine("Введите два числа");
+            int num5 = Convert.ToInt32(Console.ReadLine());
+            int num6 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(num5 * num6);
+            break;
+        case 4:
+            Console.WriteLine("Введите два числа");
+            int num7 = Convert.ToInt32(Console.ReadLine());
+            int num8 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(num7 / num8);
+            break;
+        case 5:
+            Console.WriteLine("Введите два числа");
+            int step0 = 1;
+            int step1 = Convert.ToInt32(Console.ReadLine());
+            int step2 = Convert.ToInt32(Console.ReadLine());
+            for (int step = 0; step <= step2; step++)
+            {
+                step0 = step1 * step1;
+            }
+            Console.WriteLine(step0);
+            break;
+        case 7:
+            int sot = 100;
+            Console.WriteLine("Введите число");
+            float num12 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(num12 / sot);
+            break;
+        case 8:
+            Console.WriteLine("Введите число");
+            int result = 1;
+            int fact = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= fact; i++)
+            {
+                result = result * i;
+            }
+            Console.WriteLine(result);
+            break;
+        case 9:
+            Console.WriteLine("Завершить программу.");
+            break;
+    }
+} while (oper != 9);
